9033a7bca8bb31445685f5827684e3072de09373
Felipe Bueno de Souza <felipebuenosouza@uol.com.br>
2022-11-06 19:12:20 -0300

    arrumando docker compose

--- first-project/src/test/java/com/first/first/FirstApplicationTests.java
+++ first-project/src/test/java/com/first/first/FirstApplicationTests.java
@@ -12,4 +12,13 @@ class FirstApplicationTests {
         System.out.println("Roudou a func de testes do primeiro projeto!");
     }
 
+    @Test
+    void printsToSee() {
+
+        Integer i;
+        for (i = 0; i < 10; i++) {
+            System.out.println("\nLinha: ".concat(i.toString()));
+        }
+    }
+
 }
