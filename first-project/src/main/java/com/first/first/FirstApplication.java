d8a7c7d258283c2fdb841a5a25482732a9b305e8
Felipe Bueno de Souza <felipebuenosouza@uol.com.br>
2022-11-06 17:49:23 -0300

    adicioando e arrumadno projetos

---
+++ first-project/src/main/java/com/first/first/FirstApplication.java
@@ -0,0 +1,31 @@
+package com.first.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FirstApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello WRLD";
+    }
+
+    @RequestMapping("/first")
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void teste() {
+        System.out.println("Primiro projeto online");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApplication.class, args);
+    }
+
+}
